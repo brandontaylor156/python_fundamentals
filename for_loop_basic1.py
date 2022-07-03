54a2cf8600e38c9d91c4b697f55e97bb01753b28
brandontaylor156 <brandon.david4@outlook.com>
2022-07-02 22:08:58 -0700

    first commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,55 @@
+# 1. Basic - Print all integers from 0 to 150.
+
+def print0to150():
+    for i in range(0,151):
+        print(i)
+
+print0to150()
+
+# 2. Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+
+def printMultiplesOf5():
+    for i in range(5, 1001, 5):
+        print(i)
+
+printMultiplesOf5()
+
+# 3. Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+
+def printDojoWay():
+    for i in range(1, 101):
+        if i % 10 == 0:
+            print("Coding Dojo")
+        elif i % 5 == 0:
+            print("Coding")
+        else:
+            print(i)
+
+printDojoWay()
+
+# 4. Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+
+def hugeSucker():
+    sum = 0
+    for i in range(1, 500001, 2):
+        sum += i
+    print(sum)
+
+hugeSucker()
+
+# 5. Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+def countdownByFours():
+    for i in range(2018, 0, -4):
+        print(i)
+    
+countdownByFours()
+
+# 6. Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+def flexibleCounter(lowNum, highNum, mult):
+    for i in range(lowNum, highNum+1):
+        if i % mult == 0:
+            print(i)
+
+flexibleCounter(2, 29, 4)
